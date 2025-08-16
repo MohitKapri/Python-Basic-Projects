ee8261de65497822355319372c0abe521648588a
Mohit kapri <mohitkapri154@gmail.com>
2025-08-16 11:14:53 +0530

    Add files via upload

---
+++ 02-The_Perfect_guess/main.py.py
@@ -0,0 +1,19 @@
+import random
+n = random.randint(1 , 100)
+a = -1
+guesses =0
+
+while(a!= n):
+    
+    a = int(input("Guess the Number:"))
+
+    if(a>n):
+        print("Lower number please")
+        guesses +=1
+
+    elif(a<n):
+        print("Higher number please")
+        guesses +=1
+
+
+print(f"You have gussed the number {n} correctly in {guesses} attempts")
